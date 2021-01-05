1963cd83864bed056ecf6f8d9a78eb5c408fbc60
Parv09 <65438534+Parv09@users.noreply.github.com>
2021-01-05 20:28:34 +0800

    created hi.c
    This file contains code for the Fibonacci sequence using python.

--- hi.c
+++ hi.c
@@ -1,5 +1,16 @@
-i = 10 
-while i>0:
-    print(i)
-    i=i-2
+N = int(input("Enter range:"))
+
+i=0
+a = 0
+b = 1
+
+while i < N:
+    if i <= 1:
+        Next = i
+    else:
+        Next = a + b
+        a = b
+        b = Next
+    print(Next)
+    i = i + 1
     
